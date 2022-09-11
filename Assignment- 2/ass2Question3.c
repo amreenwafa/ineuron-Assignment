10179edbc9c3261a8e5ef4625e2e6a7ca2dc6998
Amreen <wafaamreen6@gmail.com>
2022-09-11 08:18:30 +0530

    assignment2 in updated

---
+++ Assignment- 2/ass2Question3.c
@@ -0,0 +1,20 @@
+//Write a program to swap values of two int variables
+#include<stdio.h>
+int main()
+{
+    int a,b;
+
+    printf("Enter the number");
+    scanf("%d",&a);
+
+    printf("Enter the number");
+    scanf("%d",&b);
+
+    a=a+b;
+    b=a-b;
+    a=a-b;
+
+    printf("%d %d number after swaping",a,b);
+
+return 0;
+}   
