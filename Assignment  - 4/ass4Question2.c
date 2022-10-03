a831c880aa4890de9d35ff9a26d8af8d0fcdab6e
Amreen <wafaamreen6@gmail.com>
2022-10-03 20:05:21 +0530

    updated

---
+++ Assignment  - 4/ass4Question2.c
@@ -0,0 +1,12 @@
+//2. Write a program to print the first 10 natural numbers.
+#include<stdio.h>
+int main()
+{
+    int a=1;
+    while(a<=10)
+    {
+        printf("%d \t",a);
+        a++;
+    }
+return 0;
+}
