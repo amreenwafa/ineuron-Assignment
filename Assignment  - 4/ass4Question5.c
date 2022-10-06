fd139cfc33353cc98c0b6082218d001d1115755c
Amreen <wafaamreen6@gmail.com>
2022-10-06 10:36:30 +0530

    updated

---
+++ Assignment  - 4/ass4Question5.c
@@ -0,0 +1,13 @@
+//5. Write a program to print the first 10 odd natural numbers in reverse order
+#include<stdio.h>
+int main()
+{
+    int a=20;
+    while(a>=1)
+    {
+        if(a%2!=0)
+           printf("%d \t",a);
+            a--;
+    }
+return 0;
+}
