0dcd6e679288eaedd942640393bba79163e177dd
Amreen <wafaamreen6@gmail.com>
2022-10-06 10:19:36 +0530

    updated

---
+++ Assignment  - 4/ass4Question3.c
@@ -0,0 +1,12 @@
+//3. Write a program to print the first 10 natural numbers in reverse order
+#include<stdio.h>
+int main()
+{
+    int a=10;
+    while(a>=1)
+    {
+        printf("%d \t",a);
+        a--;
+    }
+return 0;
+}
