1ae9a021f37e33da20032ee65f809124f825804f
Amreen <wafaamreen6@gmail.com>
2022-10-06 10:34:33 +0530

    updated

---
+++ Assignment  - 4/ass4Question4.c
@@ -0,0 +1,13 @@
+//4. Write a program to print the first 10 odd natural numbers
+#include<stdio.h>
+int main()
+{
+    int a=1;
+    while(a<=20)
+    {
+        if(a%2!=0)
+           printf("%d \t",a);
+            a++;
+    }
+return 0;
+}
