cf573cd2cb8574312772040f0e459e09b746d076
Amreen <wafaamreen6@gmail.com>
2022-10-22 13:16:36 +0530

    updated

--- Assignment -6/ass6Question9.c
+++ Assignment -6/ass6Question9.c
@@ -2,6 +2,19 @@
 #include<stdio.h>
 int main()
 {
+    int n,m,i,lcm=1;
+
+      printf("Enter the number to check : ");
+      scanf("%d%d",&n,&m);
     
+       for(i=1;i<=n && i<=m ;i++)             //finding the factors of the number 
+       {
+          if(n%i==0 && m%i==0)
+                printf("\n factor of two number is=%d",i);    
+
+            lcm=lcm*i;                          
+       }
+        printf("\nLCM of two number is=%d",lcm);
+      
 return 0;
 }
