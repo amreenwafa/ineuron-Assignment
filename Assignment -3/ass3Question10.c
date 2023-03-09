e52eae26c8d632e2c2167c98f9ad72f2e3e0c405
Amreen <wafaamreen6@gmail.com>
2023-03-09 19:22:04 +0530

    updated

--- Assignment -3/ass3Question10.c
+++ Assignment -3/ass3Question10.c
@@ -5,7 +5,7 @@ int main()
 {
     int cp,sp,profit,loss,p,l;
 
-    printf("Enter the cost price : ")
+    printf("Enter the cost price : ");
     scanf("%d",&cp);
 
     printf("Enter the selling price : ");
